17e7e2f0124021a1f69a9f6ba2310b353c0cfd67
Jason Hite <hite77@gmail.com>
2018-03-15 20:00:29 -0400

    Get it to set initial state

--- lib/line.rb
+++ lib/line.rb
@@ -1,9 +1,23 @@
 class Line
-  def initialize ( set, lines )
-
+  def initialize ( set, width )
+    @elements = []
+    position = 0
+    set.each { |element|
+      @elements.push([position,element])
+      position = position + element + 1
+    }
   end
 
   def output_text
-    "x0000"
+    string = "00000"
+    @elements.each { |element|
+         counter = element[1]
+         start_position=element[0]
+         while (counter > 0) do
+           string[start_position+counter-1] = 'x'
+           counter -=1
+         end
+      }
+    string
   end
 end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -2,7 +2,12 @@ require 'line'
 
 RSpec.describe Line, "#Line represents a horizontal line" do
   it "One block and 5 width can output it's state" do
-      line = Line.new(1,5)
-      expect(line.output_text).to eq "x0000"
+    line = Line.new([1],5)
+    expect(line.output_text).to eq "x0000"
+  end
+
+  it "Set more than one grouping on a line" do
+    line = Line.new([1,2],5)
+    expect(line.output_text). to eq "x0xx0"
   end
 end
